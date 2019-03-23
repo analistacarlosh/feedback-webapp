df2d2b25a5c103144d9076a8a728d629d533e5ee
Carlos <analista.carlosh@gmail.com>
2019-03-23 15:55:02 -0300

    Basic static questions structure

--- src/App.js
+++ src/App.js
@@ -1,27 +1,73 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import React, { useState } from 'react';
 import './App.css';
+import Question from './components/Question/Question'
 
 class App extends Component {
+
+  // useState();
+
+  // Class bassed componnet - React hoks
+  // reserved word
+  // unmutable
+  state = {
+    answers: [
+        {question: 'Are you happy?', answer: 1},
+        {question: 'What about the weather?', answer: 1},
+        {question: 'Are you happy with your Job?', answer: 1},
+    ]
+  }
+
+  sendHandler = (answer) => {
+    console.log('sent::-:)');    
+    // this.state.answers[0].question;
+    this.setState({
+      answers: [
+        {question: 'Are you happy?', answer: 4},
+        {question: 'What about the weather?', answer: 3},
+        {question: 'Are you happy with your Job?', answer: 3},
+      ]
+      }
+    )
+  }
+
+  answersHandler = (event) => {
+    console.log('answersHandler::-:)');    
+    this.setState({
+      answers: [
+        {question: 'Are you happy?', answer: event.target.value},
+        {question: 'What about the weather?', answer: event.target.value},
+        {question: 'Are you happy with your Job?', answer: 3},
+      ]
+      }
+    )
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Please, answer the questions below with number from 1 to 5.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          
+          <Question
+            question={this.state.answers[0].question}
+            answer={this.state.answers[0].answer}
+            changed={this.answersHandler}
+            click={this.sendHandler.bind(this, '0')}
+          > -
+          </Question>
+
+          <p>
+            <button onClick={this.sendHandler}> Send </button>
+            <button onClick={ () => this.sendHandler}> Send </button>
+          </p>
+
         </header>
       </div>
     );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'FeedBack App'))
   }
 }
 
---
+++ src/components/Question/Question.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Question.css';
+
+const question = (props) => {
+    return (
+        <div className="Question">
+            <p> Questions:  {props.question} </p>
+            <input type="text" onChange={props.changed} value={props.answer}  />
+            
+            <p> You are feeling: {props.answer}  </p>
+        </div>
+    );
+}
+
+export default question;
